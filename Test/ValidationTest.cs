ca3825bf19c7259647f87c0bcebefdefcc211451
stafford <stafford.williams@gmail.com>
2017-09-16 09:45:51 +1000

    save test

--- Test/ValidationTest.cs
+++ Test/ValidationTest.cs
@@ -11,12 +11,21 @@ namespace Test
         [TestMethod]
         public void Validation_WhenEntityHasComputedColumn_ShouldValidate()
         {
-            var entity = new MyEntity { MyColumn = "something " };
+            var entity = new MyEntity { MyColumn = "something" };
             var context = new MyDbContext();
             context.MyEntities.Add(entity);
             context.GetValidationErrors()
                 .SelectMany(p => p.ValidationErrors).Select(p => $"{p.PropertyName} - ${p.ErrorMessage})")
                 .ShouldBeEmpty();
+        }
+
+        [TestMethod]
+        public void Saving_WhenEntityHasComputedColumn_ShouldValidate()
+        {
+            var entity = new MyEntity { MyColumn = "something" };
+            var context = new MyDbContext();
+            context.MyEntities.Add(entity);
+            Should.NotThrow(() => context.SaveChanges());
         }
     }
 }
